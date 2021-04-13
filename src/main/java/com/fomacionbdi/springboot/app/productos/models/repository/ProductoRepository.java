439b7505edf2fb1a66a9e0158b3cac83a249715c
raul <raul.lopezm@softtek.com>
2021-04-13 17:11:10 -0500

    eliminado

--- src/main/java/com/fomacionbdi/springboot/app/productos/models/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fomacionbdi.springboot.app.productos.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.fomacionbdi.springboot.app.productos.models.entity.Producto;
-
-public interface ProductoRepository extends CrudRepository<Producto, Long> {
-
-}
