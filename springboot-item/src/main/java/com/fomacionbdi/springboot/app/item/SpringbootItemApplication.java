7d3ad7edbd4c7fce2af97c7e8faced38b1dc405c
RaulLop <60266598+RaulLop@users.noreply.github.com>
2021-04-13 17:20:43 -0500

    Add files via upload

---
+++ springboot-item/src/main/java/com/fomacionbdi/springboot/app/item/SpringbootItemApplication.java
@@ -0,0 +1,19 @@
+package com.fomacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
+public class SpringbootItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootItemApplication.class, args);
+	}
+
+}
---
+++ springboot-item/src/main/java/com/fomacionbdi/springboot/app/item/service/ItemService.java
@@ -0,0 +1,12 @@
+package com.fomacionbdi.springboot.app.item.service;
+
+import java.util.List;
+
+import com.fomacionbdi.springboot.app.item.models.Item;
+
+public interface ItemService {
+	
+	public List<Item> findAll();
+	public Item findById(Long id, Integer cantidad); 
+
+}
